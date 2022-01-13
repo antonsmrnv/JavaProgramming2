87642c99580ff93fe7ffc46adc3d0b913dd1d6f1
Anton Smirnov <antonsmirnov4u@yahoo.com>
2022-01-13 11:04:05 -0500

    day38 - Inheritance

--- src/day38_Inheritance/carTask/Tesla.java
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -1,4 +1,11 @@
 package day38_Inheritance.carTask;
 
-public class Tesla {
+public class Tesla extends Car{
+    public Tesla(int year, double price, String color, int miles) {
+        super("Tesla", year, price, color, miles);
+    }
+
+    public void autoPilot(){
+        System.out.println(brand + " " +model + " is in autopilot mode");
+    }
 }
