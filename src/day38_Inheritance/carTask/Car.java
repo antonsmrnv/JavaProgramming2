c3dba152197f15f557566cc916de70f87032b236
Anton Smirnov <antonsmirnov4u@yahoo.com>
2022-01-13 10:25:54 -0500

    initial commit

--- src/day38_Inheritance/carTask/Car.java
+++ src/day38_Inheritance/carTask/Car.java
@@ -4,3 +4,31 @@ public class Car {
 
 
 }
+
+
+/*
+carTask:
+    Create a class called Car
+        instance variables:
+            brand, model, year, price, color, miles
+
+            add a constructor to set all the fields
+
+        instance methods:
+            start(), drive(),toString()
+
+    Create the following sub classes of Car:
+        1. Toyota:
+                extra methods:
+                    reliable()
+
+        2. BMW:
+                extra methods:
+                    breaksDown()
+                    racing()
+
+        3. Tesla:
+                extra methods:
+                    autoPilot();
+
+ */
