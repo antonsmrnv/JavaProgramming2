9ee2941ace63412c4516249375d270620028eb48
Anton Smirnov <antonsmirnov4u@yahoo.com>
2022-01-14 16:21:24 -0500

    day39 - Recap

---
+++ src/day39_Recap/cydeoTask/Teacher.java
@@ -0,0 +1,9 @@
+package day39_Recap.cydeoTask;
+
+public class Teacher extends Employee {
+
+
+    public Teacher(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+}
